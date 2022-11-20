a089f5f07220cb0c0078f094b43ccca191af1efb
User-1289 <109156434+User-1289@users.noreply.github.com>
2022-11-20 16:13:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//function addQuiz()
+let addQuizBtn = document.getElementById('add-quiz-id')
+addQuizBtn.onclick = () =>
+{
+	window.location.href="add-page.html"
+}
+let attemptBtn = document.getElementById('attempt-btn')
+attemptBtn.onclick = () =>
+{
+	window.location.href="join-quiz.html"
+}
