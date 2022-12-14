b67f4ba7b5c6e1d1eaf58310c113369da1bfb37b
User-1289 <109156434+User-1289@users.noreply.github.com>
2022-12-14 09:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 let addQuizBtn = document.getElementById('add-quiz-id')
 addQuizBtn.onclick = () =>
 {
-	window.open("add-page.html", "_blank")
+	window.open("add-page.html", "blank")
 }
 let attemptBtn = document.getElementById('attempt-btn')
 attemptBtn.onclick = () =>
 {
-	window.open("join-quiz.html", "_blank")
+	window.open("join-quiz.html", "blank")
 }
 let myQUizBtn = document.getElementById('my-quiz-btn')
 myQUizBtn.onclick = () =>
 {
-	window.open("my-quiz.html", "_blank")
+	window.open("my-quiz.html", "blank")
 }
